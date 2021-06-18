5c896d5b5d117425336738d80f045ab075fa308d
Quentin Vaisala <quentin@celinedesign.com>
2021-06-18 12:33:04 +0200

    integration celinedesign

---
+++ _data/sites/celinedesign-projects.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: "CelineDesign Projects", // optional, falls back to object key
+	description: "CelineDesign projects",
+	options: {
+		frequency: 60 * 20, // (in minutes), 23 hours
+	},
+	urls: [
+        "https://www.bijouteriebarriere.com/",
+		"https://www.aude-a-lamour.com/",
+        "https://www.bijouterieguilhem.com/",
+        "https://www.francefinanceconsultants.fr/",
+        "https://www.gites-lafourcade.fr/",
+        "https://www.hmaheo.com/fr/",
+        "https://www.la-belle-en-cuisse.com/",
+        "https://www.lesopticiensdoc.com/",
+        "https://massaintremy.com/",
+        "https://www.pepinieres-frances.com/",
+        "https://www.prestia-conseils.fr/",
+        "https://www.sobat.fr/",
+        "https://www.soulages-batp.com/", 
+        "https://villagecastigno.com/", 
+	]
+};
---
+++ _data/sites/celinedesign.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "CelineDesign Website", // optional, falls back to object key
+	description: "CelineDesign Website",
+	options: {
+		frequency: 60 * 20, // (in minutes), 23 hours
+	},
+	urls: [
+        "https://www.celinedesign.com/", 
+	]
+};
